a2e66f1401a7a6dd4b0dd5eee139857fb01db248
Actam55 <l.1999@hotmail>
2025-05-17 00:53:38 +0200

    added ignore

--- magicdl/obj/Release/net8.0/magicdl.AssemblyInfo.cs
+++ magicdl/obj/Release/net8.0/magicdl.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("magicdl")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1f59b675419d3d811cc2a2371620572fb59358")]
 [assembly: System.Reflection.AssemblyProductAttribute("magicdl")]
